595e8581dd65e6f3bcb701656039187a22d4e885
galina279 <galina279@gmail.com>
2023-01-28 20:42:18 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+int maximum = number1;
+if (number1<number2){
+    maximum = number2;
+}
+Console.WriteLine($"Максимальное из двух чисел - {maximum} ");
