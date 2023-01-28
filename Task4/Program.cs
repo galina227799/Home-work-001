15a4ac9ddebadf4d7221f860f6eb3714d8b8d51f
galina279 <galina279@gmail.com>
2023-01-28 20:33:00 +0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int A1 = 2;
+while(A1 <= number){
+    Console.Write($"{A1}, ");  
+    A1=A1+2; 
+}
