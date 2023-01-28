3c9fd50949d08afc337cbde9429cbcd9716c5cdf
galina279 <galina279@gmail.com>
2023-01-28 20:32:24 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+int maximum = number1;
+if (number2>maximum){
+    maximum = number2;
+}
+if(number3>maximum){
+    maximum = number3; 
+}
+Console.WriteLine($"Максимальное из трех чисел - {maximum} ");
