c4b3f7d6472b2f2853d5c399118e431974b01729
galina279 <galina279@gmail.com>
2023-01-28 20:32:44 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+int Ostatok = number1%2;
+if (Ostatok >0){
+    Console.WriteLine("Число не четное");   
+}
+else{
+  Console.WriteLine("Число четное");  
+}
